a9007e88f8aa4b2d51493908f3fa9af956ccc86d
Kara-B <kjbutler819@verizon.net>
2022-12-08 21:50:21 -0500

    Image Path Resolved

--- final/admin/recipes/index.php
+++ final/admin/recipes/index.php
@@ -64,11 +64,11 @@ $result = mysqli_query($db_connection, $query);
               $site_url = site_url();
               while ($recipes = mysqli_fetch_array($result)) {
                 echo "<tr class='allRecipesBlock'>
-                  <td class='allRecipesImage'>{$recipes['image_path']}</td>
+                  <td class='allRecipesImage'> <img src='{$site_url}/{$recipes['image_path']}' width='200px' height='150px' alt=''> </td> 
                   <td class='allRecipesName'>{$recipes['recipe_name']}</td>
                   <td class='allRecipesDescription'>{$recipes['description']} </td> 
                   <td class='allRecipesButtons'>
-                    <a href='{$site_url}/admin/recipes/edit.php?id={$recipes['id']}'><img src=' <?php echo site_url(); ?>/dist/images/edit-icon.svg'></a>
+                    <a href='{$site_url}/admin/recipes/edit.php?id={$recipes['id']}'><img src='<?php echo site_url(); ?>/dist/images/edit-icon.svg'></a>
                     <a href='{$site_url}/admin/recipes/delete.php?id={$recipes['id']}' class=''>Delete</a>
                   </td>
                 </tr>";
